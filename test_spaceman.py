f83bc2161c726bc66ca5777294e410dc8503705e
Aleia Knight <aleiaknight@gmail.com>
2019-09-18 16:34:35 -0700

    added test functions

--- test_spaceman.py
+++ test_spaceman.py
@@ -1,16 +1,27 @@
-from spaceman import *
-import unittest
+from spaceman import is_guess_in_word, get_guessed_word, is_word_guessed
+import pytest
 
-class TestSpaceman(unittest.TestCase):
+#automatically runs tests for is_guess_in_word
+def test_is_guess_in_word():
+    assert is_guess_in_word('c', 'ficticious') == True
+    assert is_guess_in_word('$', 'ficticious') == False
+    assert is_guess_in_word('1', 'ficticious') == False
+    assert is_guess_in_word('a', 'ficticious') == False
+    assert is_guess_in_word('C', 'ficticious') == True
 
-    def is_guess_in_word(self):
-        pass
+#automatically runs tests for get_guessed_word
+def test_get_guessed_word():
+    assert get_guessed_word('applesauce', 'applesauses') == False
+    assert get_guessed_word('applesause', 'applesauce') == True
+    assert get_guessed_word('applesauce', 'pplesuce') == False
 
+#automatically runs tests for is_word_guessed
+def test_is_word_guessed():
+    assert is_word_guessed('waterfall', 'waterfall') == True 
+    assert is_word_guessed('waterfall', 'waterfal') == False
+    assert is_word_guessed('waterfall', 'wtrfll') == False
 
-
-    def load_word(self):
-        pass
-
-
-    def is_word_guessed(self):
-        pass
+if '__name__' == '__main__':
+    test_is_guess_in_word()
+    test_get_guessed_word()
+    test_is_word_guessed()
