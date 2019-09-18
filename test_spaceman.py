5bb7b22dab344e80bd3b9f19c94f9d3e72f82681
Aleia Knight <aleiaknight@gmail.com>
2019-09-18 15:09:11 -0700

    Setup for test functions

---
+++ test_spaceman.py
@@ -0,0 +1,16 @@
+from spaceman import *
+import unittest
+
+class TestSpaceman(unittest.TestCase):
+
+    def is_guess_in_word(self):
+        pass
+
+
+
+    def load_word(self):
+        pass
+
+
+    def is_word_guessed(self):
+        pass
